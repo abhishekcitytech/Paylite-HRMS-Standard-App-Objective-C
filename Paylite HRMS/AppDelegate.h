d959a9d28f59c742e6e8509f756433303517483e
Chayan <chayan@mac-mini.local>
2016-03-29 18:13:28 +0530

    Initial Commit

---
+++ Paylite HRMS/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Paylite HRMS
+//
+//  Created by SANDIPAN on 29/03/16.
+//  Copyright © 2016 SANDIPAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Paylite HRMS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Paylite HRMS
+//
+//  Created by SANDIPAN on 29/03/16.
+//  Copyright © 2016 SANDIPAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
